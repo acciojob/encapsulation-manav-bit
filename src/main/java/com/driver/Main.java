ff11389e807241f07a2f2d791577b4c067137a51
MANAV AHUJA <ahujamanav43@gmail.com>
2024-02-11 23:27:04 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String args[]){
+      RWOnly obj=new RWOnly();
+//      obj.name="manav";
+//      System.out.print(obj.name);
+//      'name' has private access in 'com.driver.RWOnly'
+//      'name' has private access in 'com.driver.RWOnly'/
+      obj.setters("manav");
+      System.out.print(obj.getter());
+  }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+    public String getter(){
+        return name;
+
+    }
+    public void setters(String value){
+        this.name=value;
+    }
 }
